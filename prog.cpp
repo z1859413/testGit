7930ace6d4bd476f08110f414b2f2103eb889ac7
Brianno Coller <bdcoller@gmail.com>
2016-03-17 19:31:29 -0500

    Added more lines to the program.

--- prog.cpp
+++ prog.cpp
@@ -2,9 +2,22 @@
 
 using namespace std;
 
+double addFour(double x);
+
 int main()
 {
 	cout << "Here is an example program to test Git/Github." << endl;
+	cout << "A second line." << endl;
+	cout << "And a third line." << endl;
+
+	double p = 3.6;
+	cout << "Running the function addFour on " << p << " results in " <<
+		addFour(p) << "." << endl;
 
 	char q = cin.get();
+}
+
+double addFour(double x)
+{
+	return(x + 4.0);
 }
